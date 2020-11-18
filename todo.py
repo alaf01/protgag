56c7730b08e2470d0ab579bd88e42c53122c083b
Ala <alicja.filipek@innokrea.pl>
2020-11-18 21:07:49 +0100

    both table and list collaborations, almost ready

--- todo.py
+++ todo.py
@@ -1,7 +1,8 @@
 # odstep na home -done
 #wysokosc 2 obrazkow w research -> odstepy -done
-# obrazki dla projektow
-# nie justify przy members
-# group leader w drugiej linijce
+# obrazki dla projektow -done
+# nie justify przy members -done
+# group leader w drugiej linijce -done
 # Bandiera giapponica!
 # Downloads: scrollable
+
