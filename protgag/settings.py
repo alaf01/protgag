7e01678617f6b458f8d1bffffbfd25f5371e867a
Ala <alicja.filipek@innokrea.pl>
2020-12-11 23:29:48 +0100

    removed unnecessary

--- protgag/settings.py
+++ protgag/settings.py
@@ -129,7 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-itSTATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #
 # # Heroku: Update database configuration from $DATABASE_URL.
 # # import dj_database_url
