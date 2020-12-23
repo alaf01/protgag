20e00d7e5356109c7b0024da91ce545b3a7d643f
Ala <alicja.filipek@innokrea.pl>
2020-12-23 23:40:16 +0100

    debug false

--- protgag/settings.py
+++ protgag/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = 'False'
 
 ALLOWED_HOSTS = ['comp-gag.herokuapp.com', 'www.comp-gag.org', 'localhost']
 CSRF_COOKIE_SECURE = True
