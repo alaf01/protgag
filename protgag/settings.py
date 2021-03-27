7fd8691da5385f0f5dd8c04c6b46384bcf0bad68
Ala <alicja.filipek@innokrea.pl>
2021-03-27 13:02:00 +0100

    debug from os environ in settings

--- protgag/settings.py
+++ protgag/settings.py
@@ -13,6 +13,7 @@ import django_heroku
 import os
 import dj_database_url
 import dotenv
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +27,7 @@ if os.path.isfile(dotenv_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'False'
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['comp-gag.herokuapp.com', 'www.comp-gag.org', 'localhost']
 CSRF_COOKIE_SECURE = True
